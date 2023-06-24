f98bcfce39bac4d4caf170aa914a0c7b182a6436
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2023-06-24 11:48:18 +0700

    fix: multiple request bugs

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -21,7 +21,7 @@
 		<h2 class="card-title">{name}</h2>
 		<p>{artist}</p>
 		<div class="card-actions justify-end">
-			<button class="btn btn-primary btn-circle" on:click={enqueue(data)}>
+			<button class="btn btn-primary btn-circle" on:click={enqueue({...data})}>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					class="h-6 w-6"
--- src/lib/webSocketConnection.ts
+++ src/lib/webSocketConnection.ts
@@ -1,5 +1,6 @@
 import ioClient from 'socket.io-client';
 
+// const ENDPOINT = 'https://musickit.wit03.xyz';
 const ENDPOINT = 'https://musickit.wit03.xyz';
 const socket = ioClient(ENDPOINT);
 
