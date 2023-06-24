310a9442c7433b909b73a066a1823a19f42c8c30
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2023-06-24 10:42:15 +0700

    feat: new url

--- src/lib/webSocketConnection.ts
+++ src/lib/webSocketConnection.ts
@@ -1,6 +1,6 @@
 import ioClient from 'socket.io-client';
 
-const ENDPOINT = 'http://18.141.235.114:8080';
+const ENDPOINT = 'https://musickit.wit03.xyz';
 const socket = ioClient(ENDPOINT);
 
 export const io = socket;
