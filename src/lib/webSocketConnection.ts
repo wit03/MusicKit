bbc215205a43a2439df06207e102960392e0b2c3
wit03 <witjarukit@gmail.com>
2025-05-02 16:12:45 +0700

    refactor: delete everything

--- src/lib/webSocketConnection.ts
+++
@@ -1,7 +0,0 @@
-import ioClient from 'socket.io-client';
-
-// const ENDPOINT = 'https://musickit.wit03.xyz';
-const ENDPOINT = 'https://musickit.wit03.xyz';
-const socket = ioClient(ENDPOINT);
-
-export const io = socket;
